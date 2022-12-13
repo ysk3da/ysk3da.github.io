44f637fd410ebb700e93ff218b956528193e9ccf
Yusuke <y-maeda@recus-groove.com>
2022-12-13 15:36:35 +0900

    add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,43 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    colors: {
+      transparent: 'transparent',
+      black: '#0A0904',
+      white: '#fff',
+      yellow: 'FCF16E',
+      gray: {
+        100: '#f7fafc',
+        // ...
+        900: '#1a202c',
+      },
+    },
+    // spacing: {
+    //   0: '0',
+    //   0.5: '0.125rem',
+    //   1: '0.25rem',
+    // },
+    borderRadius: {
+      'none': '0',
+      'sm': '.125rem',
+      DEFAULT: '.25rem',
+      'lg': '.5rem',
+      'full': '9999px',
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // Official plugins
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
