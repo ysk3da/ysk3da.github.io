93a285a3de617e270c8ad6b7f89e0ee1723068f0
Yusuke <y-maeda@recus-groove.com>
2022-12-14 10:51:58 +0900

    temp change code

--- app/test/UsersList.tsx
+++ app/test/UsersList.tsx
@@ -4,42 +4,19 @@ type User = {
   email: string;
 };
 
-// const getUsers = async () => {
-//   const response = await fetch('https://jsonplaceholder.typicode.com/users');
-//   const users: User[] = await response.json();
-//   return users;
-// };
-
 const getUsers = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/users`
-  );
-  const users = await response.json();
-
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users: User[] = await response.json();
   return users;
 };
 
 const UsersList = async () => {
   const users = await getUsers();
-
-  const List = () => {
-    return(
-      <>
-        {users && users.map((user) => {
-          return (
-            <li key={user.id}>{user.name}</li>
-          )
-        })}
-      </>
-    )
-  }
-
   return (
     <>
       <h2 className="text-lg font-bold mt-4">ユーザ一覧</h2>
       <ul>
-        <List/>
+        {users && users.map((user) => <li key={user.id}>{user.name}</li>)}
       </ul>
     </>
   );
