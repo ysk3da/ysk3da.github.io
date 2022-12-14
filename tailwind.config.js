4d5a4ba4ffbaffb466641eed401f269d1720efd6
Yusuke <y-maeda@recus-groove.com>
2022-12-14 19:02:58 +0900

    add font family and animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,7 +36,29 @@ module.exports = {
       'lg': '.5rem',
       'full': '9999px',
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        zendots: ['var(--font-zendots)'],
+        bizudg: ['var(--font-bizudg)'],
+      },
+      animation: {
+        "tracking-in-expand": "tracking-in-expand 0.7s cubic-bezier(0.215, 0.610, 0.355, 1.000)   both"
+      },
+      keyframes: {
+          "tracking-in-expand": {
+              "0%": {
+                  "letter-spacing": "-.5em",
+                  opacity: "0"
+              },
+              "40%": {
+                  opacity: ".6"
+              },
+              to: {
+                  opacity: "1"
+              }
+          }
+      }
+    },
   },
   plugins: [
     // Official plugins
