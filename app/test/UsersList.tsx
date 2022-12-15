f992837e553081aaf714c568be6ed8d386792902
Yusuke <y-maeda@recus-groove.com>
2022-12-15 10:40:12 +0900

    comment out log

--- app/test/UsersList.tsx
+++ app/test/UsersList.tsx
@@ -8,7 +8,7 @@ const getUsers = async () => {
   await new Promise((resolve) => setTimeout(resolve, 5000));
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
   const users: User[] = await response.json();
-  console.log({users});
+  // console.log({users});
   return users;
 };
 
