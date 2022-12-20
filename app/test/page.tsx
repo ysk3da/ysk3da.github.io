50ed1a60a7088af394ed9ea740095f1701b32ec8
Yusuke <y-maeda@recus-groove.com>
2022-12-20 16:24:22 +0900

    comment out fetch component

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,4 +1,4 @@
-import UsersList from './UsersList';
+// import UsersList from './UsersList';
 import Counter from './Counter';
 import { Suspense } from 'react';
 const Test = () => {
@@ -7,7 +7,7 @@ const Test = () => {
       <h1 className="text-xl font-bold">Testページ</h1>
       <Counter />
       <Suspense fallback={<p className="mt-4">ユーザデータ　Loading...</p>}>
-        <UsersList />
+        {/* <UsersList /> */}
       </Suspense>
     </>
   );
