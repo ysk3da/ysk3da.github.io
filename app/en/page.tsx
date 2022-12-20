f334fcb8c0f462c89212bb5a4259e3d6f8e1b121
Yusuke <y-maeda@recus-groove.com>
2022-12-20 16:23:20 +0900

    add page en

---
+++ app/en/page.tsx
@@ -0,0 +1,13 @@
+/**
+ * next/router -> next/navigation の変更に伴い、
+ * localeコンポーネントが利用できないため別途対策が必要
+ */
+const En = () => {
+  return (
+    <>
+      <h1 className="text-xl font-bold">En Testページ</h1>
+    </>
+  );
+};
+
+export default En;
